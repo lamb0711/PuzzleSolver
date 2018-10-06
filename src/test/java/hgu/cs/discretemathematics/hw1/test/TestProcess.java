f15c223e84e9c26e4dd2a4f191f86d809c167869
SUJIN <21500394@handong.edu>
2018-10-06 23:56:49 +0900

    Test parsing

--- src/test/java/hgu/cs/discretemathematics/hw1/test/TestProcess.java
+++ src/test/java/hgu/cs/discretemathematics/hw1/test/TestProcess.java
@@ -1,9 +1,12 @@
 package hgu.cs.discretemathematics.hw1.test;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -25,6 +28,11 @@ public class TestProcess {
 	private void run(String[] args) {
 		Options options = createOptions();
 		String line;
+//		String buffer;
+		String [][]a = new String[9][9];//[row][column]
+		int i=0;
+		int j=0;
+		
 
 		if(parseOptions(options, args)) {
 			if (help){
@@ -33,19 +41,46 @@ public class TestProcess {
 			}
 
 			try {
-				/*
-				InputStreamReader read = new InputStreamReader(new FileInputStream(input));
-				BufferedReader b = new BufferedReader(read);
-
-				while((buffer = b.readLine()) != null) formula.add(buffer);
-				*/
 				
+//				InputStreamReader read = new InputStreamReader(new FileInputStream(input));
+//				BufferedReader b = new BufferedReader(read);
+//
+//				while((buffer = b.readLine()) != null) {
+//					a[i] = buffer.split("\\s");
+//					i++;
+//				}
+//				for(int s=0; s< 2; s++) { //input.txt의 row
+//					for(int z = 0; z<8; z++) {//column
+//						System.out.print(a[s][z]+" ");
+//					}
+//					System.out.println();
+//				}
+				
+				Pattern pattern = Pattern.compile("(.+a(.)(.))(.+\\s+(.))");
+			
 				ProcessBuilder builder = new ProcessBuilder("./z3","./formula.txt");
 				Process p = builder.start();
 				p.waitFor();
 				BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				
 				while((line = br.readLine())!=null) {
-					System.out.println(line);
+					if(line.contains("()")) {
+						line = line+(br.readLine());
+						
+						Matcher matcher = pattern.matcher(line);
+						while(matcher.find()) {
+							i = Integer.parseInt(matcher.group(2));
+							j = Integer.parseInt(matcher.group(3));
+							a[i-1][j-1] = matcher.group(5);
+						}
+					}
+				}
+				
+				for(int s=0; s< 8; s++) { //input.txt의 row
+					for(int z = 0; z<8; z++) {//column
+						System.out.print(a[s][z]+" ");
+					}
+					System.out.println();
 				}
 				
 			} catch (IOException e) {
